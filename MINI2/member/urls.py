557b82e14201e235686654d750df9f5e23febfb3
MinSik <hyunicho62@gmail.com>
2021-07-02 18:02:57 +0900

    로그인 회원가입 UI 추가

--- MINI2/member/urls.py
+++ MINI2/member/urls.py
@@ -6,4 +6,5 @@ app_name = 'member'
 urlpatterns = [
     path('login/', views.login, name='login'),
     path('register/', views.register, name='register'),
+    path('logout/', views.logout, name='logout')
 ]
--- MINI2/member/views.py
+++ MINI2/member/views.py
@@ -1,5 +1,5 @@
 from django.http.response import HttpResponse, HttpResponseRedirect
-from django.shortcuts import redirect, render, resolve_url
+from django.shortcuts import redirect, render
 from django.utils import timezone
 
 
@@ -13,31 +13,47 @@ from .models import Member
 # Create your views here.
 def register(request):
     if request.method == 'GET':
-        return render(request, 'member/register.html')
+        return render(request, 'member/reg  ister.html')
      #"POST"
     else: 
         # HTML로부터 받은 정보 확인
-        user_phone = request.POST['user_phone']
         user_name = request.POST['user_name']
         user_pw = request.POST['user_pw']
+        user_pwchk = request.POST['user_pwchk']
+        user_phone = request.POST['user_phone']
+
+        message_dict = {} # html에 던져줄 dictionary
         
-        # --------같은 이름이 없는지 확인하고 회원가입-------
+        # --------비밀번호 == 비밀번호확인 인지 체크 -------
+        # code
+        if user_pw != user_pwchk:
+            message_dict['error_pw'] = '설정한 비밀번호가 일치하지 않습니다.'
+            return render(request, 'member/register.html', {'error_pw':message_dict['error_pw'],
+                                                            'user_pw':user_pw,
+                                                            'user_name':user_name})
+        
+        # --------같은 전화번호가 없는지 확인하고 회원가입-------
         # code
         # ---------------
-
+        if user_pw != user_pwchk:
+            message_dict['error_pw'] = '설정한 전화번호이미 존재합니다.'
+            return render(request, 'member/register.html', {'error_pw':message_dict['error_pw'],
+                                                            'user_pw':user_pw,
+                                                            'user_pwchk':user_pwchk,
+                                                            'user_name':user_name})
+        # ---
         m = Member(
             user_name = user_name, user_pw=user_pw,
             user_phone = user_phone)
         m.c_date = timezone.now()
         m.save()
         messages.warning(request, '회원가입 성공')
-        return redirect('member:login') # urls.py/view-name을 적어야함
-        # return render(request, 'member/login.html')
-        # return HttpResponse('가입 완료' + user_name + user_pw + user_phone)
+        return redirect('member:register') # urls.py/view-name을 적어야함
 
 def login(request):
     if request.method == 'GET':
-        return render(request, 'member/login.html', {})
+        return render(request, 'member/register.html', {})
+     # POST
     else:
         # 받은 정보 확인
         user_phone = request.POST['user_phone']
@@ -48,7 +64,6 @@ def login(request):
                                         user_pw=user_pw)
         except:            
             return HttpResponse('로그인 실패')
-            # return render(request, 'member/login.html', {})
         else:
             # 1. DB와 확인해서 로그인 -> db에 아이디가 있다면 로그인 성공
 
@@ -61,5 +76,9 @@ def login(request):
             # code
             # -----------------------------------
             return redirect('mainpage:mainhome')
-            # return render(request, 'mainpage/mainhome.html', {})
-            # return HttpResponse('로그인 성공')
+
+def logout(request):
+    if request.session['user_phone']:
+        del request.session['user_phone']
+        return redirect('mainpage:mainhome')
+    
