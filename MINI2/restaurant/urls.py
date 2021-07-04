d77951c0bec7c08a166d527b9055681b02c299c9
김소희 <sohuig@sookmyung.ac.kr>
2021-07-05 02:38:41 +0900

    merge restaurantapp (afb8c94)

--- MINI2/restaurant/urls.py
+++ MINI2/restaurant/urls.py
@@ -6,7 +6,9 @@ from . import views
 app_name = 'restaurant'
 
 urlpatterns = [
-    path('', views.restaurant, name='rest'),
-    path('restaurant_list/', views.restaurant_list, name='rest_list'),
-    path('restaurant_detail/', views.restaurant_detail, name='rest_detail'),
+    path('', views.restaurant, name='resta'),
+    path('restaurant_list/', views.restaurant_list, name='list'),
+    path('restaurant_detail/', views.restaurant_detail, name='detail'),
+        #'<str:rest_url>/',
+    # path('restaurant_search/', views.restaurant_search),
 ]
