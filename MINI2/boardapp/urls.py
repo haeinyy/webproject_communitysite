f8b696065c372cb791aa9c9b1fce42e42e0faaa6
haeinyy <godls4402@gmail.com>
2021-07-05 02:31:18 +0900

    Update comment func

--- MINI2/boardapp/urls.py
+++ MINI2/boardapp/urls.py
@@ -9,5 +9,7 @@ urlpatterns = [
     path('freeboard/', views.freeboard, name='freeboard'),
     path('writetext/', views.writetext, name='writetext'),
     path('<int:pk>/', views.content_view, name='content_view'),
+    path('comment_write/<int:pk>/', views.comment_write, name='comment_write'),
+    path('like/<int:post_id>/', views.like_post, name="like_post"),
     # path('freeboard/<int:pk>', views.content_view, name='content_view'),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
