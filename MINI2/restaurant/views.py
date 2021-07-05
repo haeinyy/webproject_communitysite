efd211dd339f1e6a08621a1c7ad03e58a84a143b
SonJi0507 <jhson0507@gmail.com>
2021-07-05 18:26:07 +0900

    21 07 05 작업

--- MINI2/restaurant/views.py
+++ MINI2/restaurant/views.py
@@ -132,7 +132,7 @@ def restaurant_detail(request): # = None):
                 rest_starscore = starscore
                 #입력 안된 것 : seenum, rmd
             )
-            if 'rest_img' in locals() :
+            if 'rest_img1' in locals() :
                 rest.rest_img1 = rest_img1
             if 'rest_img2' in locals() :
                 rest.rest_img2 = rest_img2
